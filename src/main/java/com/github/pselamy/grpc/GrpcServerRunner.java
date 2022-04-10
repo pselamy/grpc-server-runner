dcd9802a00dca819d4deb5a746a2c123ae6e9b00
Patrick Selamy <pselamy@gmail.com>
2022-04-10 11:27:02 -0500

    Create GrpcServerRunner.java

---
+++ src/main/java/com/github/pselamy/grpc/GrpcServerRunner.java
@@ -0,0 +1,71 @@
+package com.github.pselamy.grpc;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
+import io.grpc.BindableService;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.ServerServiceDefinition;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.lang.Integer.parseInt;
+
+/**
+ * Manages startup/shutdown of a gRPC server.
+ */
+public class GrpcServerRunner {
+    private static final Logger logger = Logger.getLogger(GrpcServerRunner.class.getName());
+
+    private final Server server;
+
+    @Inject
+    GrpcServerRunner(PortSupplier portSupplier, Set<BindableService> services) {
+        this.server = ServerBuilder
+                .forPort(portSupplier.get())
+                .addServices(getServiceDefinitions(services))
+                .build();
+    }
+
+    private ImmutableList<ServerServiceDefinition> getServiceDefinitions(Set<BindableService> services) {
+        return services.stream()
+                .map(BindableService::bindService)
+                .collect(toImmutableList());
+    }
+
+    public void run() throws InterruptedException {
+        start();
+        server.awaitTermination();
+    }
+
+    private void start() {
+        logger.info("Server started, listening on " + server.getPort());
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            try {
+                GrpcServerRunner.this.stop();
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.err);
+            }
+            System.err.println("*** server shut down");
+        }));
+    }
+
+    private void stop() throws InterruptedException {
+        server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    }
+
+    static class PortSupplier implements Supplier<Integer> {
+        @Override
+        public Integer get() {
+            String port = System.getenv()
+                    .getOrDefault("PORT", "50051");
+            return parseInt(port);
+        }
+    }
+}
